87bd315364a5d37fd746df96bcc491115e294a6e
lccarrillo247 <lccarrillo247@gmail.com>
2023-10-26 12:11:05 -0500

    Quitar adminnavbar de app

--- src/Pages/Admin/AdminHome/AdminHome.jsx
+++ src/Pages/Admin/AdminHome/AdminHome.jsx
@@ -3,7 +3,8 @@ import styles from './AdminHome.module.css';
 
 export default function AdminHome() {
     return (
-        <div>
+        <div className={styles.AdminHomeContainer}>
+            <div></div>
             <h1>Admin Home :{")"}</h1>
         </div>
     )
