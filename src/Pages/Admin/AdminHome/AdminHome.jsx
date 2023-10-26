49789016903c0ce22c6e242d980fe7c4a344d20e
lccarrillo247 <lccarrillo247@gmail.com>
2023-10-26 13:27:34 -0500

    Sección adminNavbar y adminComponents

--- src/Pages/Admin/AdminHome/AdminHome.jsx
+++ src/Pages/Admin/AdminHome/AdminHome.jsx
@@ -1,15 +1,32 @@
 // Luissssss
+import AdminEvents from '../../../Components/Admin/AdminEvents/AdminEvents';
+import AdminEventsCreate from '../../../Components/Admin/AdminEventsCreate/AdminEventsCreate';
 import AdminNavbar from '../../../Components/Admin/AdminNavbar/AdminNavbar';
+import AdminUsers from '../../../Components/Admin/AdminUsers/AdminUsers';
 import styles from './AdminHome.module.css';
+import { useState } from 'react';
 
 export default function AdminHome() {
+
+    const [display, setDisplay] = useState({
+        show: "home"
+    });
+
+    console.log(display)
+
     return (
         <div className={styles.AdminHomeContainer}>
             <div>
-            <AdminNavbar />
+            <AdminNavbar
+            display={display}
+            setDisplay={setDisplay}
+            />
             </div>
             <div>
-            <h1>Admin Home :{")"}</h1>
+            {display.show === "home" ? <h1>Admin Home :{")"}</h1> : null}
+            {display.show === "showEvents" ? <AdminEvents /> : null}
+            {display.show === "createEvents" ? <AdminEventsCreate /> : null}
+            {display.show === "showUsers" ? <AdminUsers /> : null}
             </div>
         </div>
     )
