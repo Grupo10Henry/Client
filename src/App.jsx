4b0a944f0ad1ee0ef13ab8b7729ea11bd04edec7
JE-FUNES <infoventacba@gmail.com>
2023-10-26 23:07:42 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,11 @@ import Navbar from "./Components/User/Navbar/Navbar"
 import AdminNavbar from './Components/Admin/AdminNavbar/AdminNavbar';
 import MyAccount from './Pages/User/MyAccount/MyAccount';
 import Login from './Components/User/Login/Login';
-import Detail from "./Pages/User/Detail/Detail";
 import AdminHome from './Pages/Admin/AdminHome/AdminHome';
 import SignUp from "./Components/User/SignUp/SignUp"
-import AdminHome from "./Pages/Admin/AdminHome/AdminHome"
 import Booking from "./Pages/User/Booking/Booking"
 import Detail from "./Pages/User/Detail/Detail"
 import Home from "./Pages/User/Home/Home"
-import MyAccount from "./Pages/User/MyAccount/MyAccount"
 
 
 function App() {
