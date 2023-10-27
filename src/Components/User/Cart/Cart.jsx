aeb7cfa93af0e7d412d8a3641d1d03d077889355
JE-FUNES <infoventacba@gmail.com>
2023-10-27 04:31:58 -0300

    Update Cart.jsx
    carrito con posibilidad de demo listo

--- src/Components/User/Cart/Cart.jsx
+++ src/Components/User/Cart/Cart.jsx
@@ -16,9 +16,9 @@ const Cart = () => {
     useEffect(() => {
     // Datos ficticios de eventos
     const eventosFicticios = [
-        { id: 1, title: 'Evento 1', price: 25000, quantity: 3 },
-        { id: 2, title: 'Evento 2', price: 30000, quantity: 2 },
-        { id: 3, title: 'Evento 3', price: 50000, quantity: 1 },
+        { id: 1, title: 'Evento 1', price: 25000, quantity: 0 },
+        { id: 2, title: 'Evento 2', price: 30000, quantity: 0 },
+        { id: 3, title: 'Evento 3', price: 50000, quantity: 0 },
     ];
         // Agrega los eventos ficticios al carrito
         setCart(eventosFicticios);
@@ -34,9 +34,16 @@ const Cart = () => {
         getCart();
     }, [id]);*/
 
-    const deleteItem = async (id) => {
+    /*const deleteItem = async (id) => {
         await axios.delete(`http://localhost:5173/api/cart/${id}`);
         setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+    };*/
+
+    const deleteItem = (id) => {
+        const confirmDelete = window.confirm('¿Estás seguro de eliminar este evento?');
+        if (confirmDelete) {
+            setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+        }
     };
 
     /*const updateItem = async (id, newQuantity) => {
@@ -57,8 +64,8 @@ const Cart = () => {
         getTotal();
     };
     
-    const handleQuantityChange = (id, newQuantity) => {
-        const updatedQuantity = cart.find((item) => item.id === id).quantity + newQuantity;
+    const handleQuantityChange = (id, change) => {
+        const updatedQuantity = cart.find((item) => item.id === id).quantity + change;
         if (updatedQuantity >= 0) {
             updateItem(id, updatedQuantity);
         }
@@ -69,6 +76,10 @@ const Cart = () => {
             return prev + item.price * item.quantity;
         }, 0);
         setTotal(total);
+        const totalEntradas = cart.reduce((prev, item) => {
+            return prev + item.quantity;
+        }, 0);
+        setTotalEntradas(totalEntradas);
     };
 
     const getTotalEntradas = () => {
@@ -80,51 +91,53 @@ const Cart = () => {
 
     return (
         <>
-      <div className="fixed inset-0 z-1000 flex items-center justify-center backdrop-filter backdrop-blur-lg">
-      {/* Este div se utilizará para el fondo desenfocado */}
-        </div>
-        <div style={{ marginTop: '5rem' }} className="fixed inset-0 z-1001 flex flex-col items-center justify-center">
-      {/* Contenedor principal del formulario */}
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm p-6 space-y-0 bg-white rounded-lg shadow-lg">
-                <img className="mx-auto h-8 w-auto" src={logo} alt="Mi Butaca" />
-            <h1 className="text-2xl text-gray-900">Carrito de compras</h1>
-            <div>
-                {cart.map((item) => (
-                    <div key={item.id}>
-                        <h1 className="text-xl text-gray-900">{item.title}</h1>
-                        <h1 className="text-lg text-gray-700">${item.price}</h1>
-                        <input
-                            type="number"
-                            value={item.quantity}
-                            onChange={(e) => updateItem(item.id, parseInt(e.target.value))}
-                            onKeyUp={(e) => {
-                                if (e.key === "ArrowUp"){
-                                    handleQuantityChange(item.id, 1);
-                                } else if (e.key === "ArrowDown"){
-                                    handleQuantityChange(item.id, -1);
-                                }
-                            }}
-                            min="0"
-                            />
-                        <button onClick={() => deleteItem(item.id)} className="bg-red-500 text-white">
-                            ❌
-                        </button>
-                       
-                    </div>
-                )
-                )}
-                <h1 className="text-lg text-gray-900">Total: ${total}</h1>
-                <h1 className="text-lg text-gray-900">Total entradas: {totalEntradas}</h1>
-                <a href="/">
-                Seguir comprando
-                </a> 
-                
-                <button className="bg-blue-500 text-white">Pagar</button>
+            <div className="fixed inset-0 z-1000 flex items-center justify-center backdrop-filter backdrop-blur-lg">
+                {/* Este div se utilizará para el fondo desenfocado */}
             </div>
-        </div>
-        </div>
+            <div style={{ marginTop: '5rem' }} className="fixed inset-0 z-1001 flex flex-col items-center justify-center">
+                {/* Contenedor principal del formulario */}
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm p-6 space-y-0 bg-white rounded-lg shadow-lg">
+                    <img className="mx-auto h-8 w-auto" src={logo} alt="Mi Butaca" />
+                    <h1 className="text-2xl text-teal-600 text-center">Carrito de compras</h1>
+                    <div className="flex flex-col">
+                        {cart.map((item) => (
+                            <div key={item.id}>
+                                <h1 className="text-xl text-gray-900">{item.title}
+                                <h2 className="text-lg font-bold text-fuchsia-500">${item.price}</h2></h1>
+                                <input
+                                    type="number"
+                                    value={item.quantity}
+                                    onChange={(e) => updateItem(item.id, parseInt(e.target.value))}
+                                    onKeyUp={(e) => {
+                                        if (e.key === "ArrowUp") {
+                                            handleQuantityChange(item.id, 1);
+                                        } else if (e.key === "ArrowDown") {
+                                            handleQuantityChange(item.id, -1);
+                                        }
+                                    }}
+                                    min="0"
+                                    className="relative bg-white w-25"
+                                    style={{ marginLeft: '7rem' }}
+                                />
+                                <button onClick={() => deleteItem(item.id)} className="bg-fuchsia-200 text-white rounded-full">
+                                    ❌
+                                </button>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="flex flex-col items-center">
+                    <h1 className="text-lg font-bold text-fuchsia-500 text-center">Total: ${total}</h1>
+                    <h1 className="text-lg text-gray-900 text-center">Total entradas: {totalEntradas}</h1>
+                    <a href="/" className="font-bold text-purple-600 hover:text-teal-600 ">
+                        Seguir comprando
+                    </a>
+                    <button className="w-32 bg-teal-600 text-white hover:bg-fuchsia-600">Pagar</button>
+                    </div>
+                </div>
+            </div>
         </>
     );
+    
 };
 
 export default Cart;
