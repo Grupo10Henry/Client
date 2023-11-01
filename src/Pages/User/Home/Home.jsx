aafc0b777d3daf86dc9c6be37d9486cae7ff67b1
Kevin Ortiz <kevinor03@gmail.com>
2023-11-01 17:42:38 -0300

    retoques y preparacion de NextEvents

--- src/Pages/User/Home/Home.jsx
+++ src/Pages/User/Home/Home.jsx
@@ -7,7 +7,7 @@ import EventsSections from "../../../Components/User/Events/EventsSections/Event
 import EventsText from "../../../Components/User/EventsText/EventsText"
 import Filters from "../../../Components/User/Filters/Filters"
 import { instance } from "../../../axios/config"
-import { getAllEvents, setEventsDate } from "../../../redux/eventsSlice"
+import { getAllEvents, setEventsDate, getNextEvents } from "../../../redux/eventsSlice"
 import EventsFiltered from "../../../Components/User/Events/EventsFiltered/EventsFiltered"
 
 import style from "./Home.module.css"
@@ -33,7 +33,7 @@ const Home = () => {
     return newDates
   }
 
-  //get events
+  //obtiene los eventos
   const getEvents = async () => {
     try {
       const { data } = await instance.get("/event") // http://localhost:3001/event
@@ -43,9 +43,20 @@ const Home = () => {
       console.log(error?.response?.data.error || error)
     }
   }
+  //obtiene los proximos eventos
+  const NextEvents = async () => {
+    try {
+      const { data } = await instance.get("/event/next") // http://localhost:3001/event
+      dispatch(setEventsDate(convertUniquesDates(data)))
+      return data
+    } catch (error) {
+      console.log(error?.response?.data.error || error)
+    }
+  }
 
   useEffect(() => {
     getEvents().then((data) => dispatch(getAllEvents(data)))
+    NextEvents().then((data) => dispatch(getNextEvents(data)))
   }, [])
 
   return (
