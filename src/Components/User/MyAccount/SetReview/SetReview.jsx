ca317f61bad55ffc7c3c318bc18354bec1cf91ef
lccarrillo247 <lccarrillo247@gmail.com>
2023-11-05 16:22:56 -0500

    CSS MyTickets V01

---
+++ src/Components/User/MyAccount/SetReview/SetReview.jsx
@@ -0,0 +1,8 @@
+// Luiiisssss
+import styles from './SetReview.module.css';
+
+export default function SetReview () {
+    return (
+        <div className={styles.abc}>Califica tu experiencia</div>
+    )
+}
