772b519c7fb6348c4fd0b78b0e34e139a3e777d4
lccarrillo247 <lccarrillo247@gmail.com>
2023-11-07 11:34:17 -0500

    Sección Reports de Admin

---
+++ src/Components/Admin/AdminReports/AdminReports.jsx
@@ -0,0 +1,11 @@
+import styles from './AdminReports.module.css'
+
+export default function AdminReports () {
+
+    return (
+        <div>
+            <h1 className={styles.reportTitle}>Admin reports</h1>
+        </div>
+    )
+
+};
