e9ef24591442ea833da5e0bb9300f412ce68f6bd
lccarrillo247 <lccarrillo247@gmail.com>
2023-11-10 06:59:59 -0500

    Edit events v01

--- src/Components/Admin/AdminEventsDetail/AdminEventsDetail.jsx
+++ src/Components/Admin/AdminEventsDetail/AdminEventsDetail.jsx
@@ -1,11 +1,36 @@
 // Luiiiis...
+import { useDispatch } from 'react-redux'
 import styles from './AdminEventsDetail.module.css'
+import { useParams } from 'react-router-dom';
+import { instance } from '../../../axios/config';
+import { useEffect, useState } from 'react';
 
 export default function AdminEventsDetail () {
 
+    const dispatch = useDispatch();
+    const params = useParams();
+
+    const [updatedEvent, setUpdatedEvent] = useState();
+
+
+    const getEvent = async () => {
+        try {
+            const {data} = await instance.get(`event/${params.id}`)
+            console.log(data)
+            return data
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    useEffect(() => {
+        getEvent()
+    }, []
+    );
+
     return (
-        <div>
-            <h1 className={styles.EventsDetailTitle}> Este componente va a estar muy difíciiiiiil :/</h1>
+        <div className={styles.editEventContainer}>
+            <h1 className={styles.EventsDetailTitle}> Edita el evento creado</h1>
         </div>
     )
 
