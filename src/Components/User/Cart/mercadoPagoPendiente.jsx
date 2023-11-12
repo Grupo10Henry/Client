8cf2bbadb1f52a5eba0163df1deefddbf0514b5a
JE-FUNES <infoventacba@gmail.com>
2023-11-12 13:32:35 -0300

    paginas mercadopago

--- src/Components/User/Cart/mercadoPagoPendiente.jsx
+++ src/Components/User/Cart/mercadoPagoPendiente.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import logo from "../../../assets/logo_mi_butaca_color.svg";
+import { useSelector } from "react-redux";
+import {Link} from "react-router-dom";
+import styles from "./mercadoPago.module.css";
 
 const MercadoPagoPendiente = () => {
+    const userName = useSelector((state) => state.user.userInfo.name);
+
+
     return (
-        <div>
-        <h1>Error en la Compra</h1>
+        <div className={styles.Page}>
+        <div className={styles.ContainerGral}>
+            <img className="mx-auto h-12 w-auto" src={logo} alt="Mi Butaca" />
+        <h1>
+            <span className={styles.Name}>{userName}</span>
+            {", "} el proceso de pago está pendiente. </h1> 
+        <h3>Espera la confirmación por email por favor. </h3>
+        <h3> Ir a: {"   "}
+            <span className={styles.Micuenta}> <Link to="/micuenta/:id"> Mi Cuenta </Link> </span>{" | "}
+            <span className={styles.Micuenta}> <Link to="/"> Inicio </Link> </span>
+           </h3>
+                  </div>
         </div>
     );
     }
