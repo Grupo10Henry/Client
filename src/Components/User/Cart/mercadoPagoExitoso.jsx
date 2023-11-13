cf5913e83dfffc2568bd6199d7927b8808c8a2bd
JE-FUNES <infoventacba@gmail.com>
2023-11-13 07:08:11 -0300

    ajuste de nombre mercadoPago

--- src/Components/User/Cart/mercadoPagoExitoso.jsx
+++ src/Components/User/Cart/mercadoPagoExitoso.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "../../../assets/logo_mi_butaca_color.svg";
 import { useSelector } from "react-redux";
 import {Link} from "react-router-dom";
-import styles from "./mercadopago.module.css";
+import styles from "./mercadoPago.module.css";
 
 const MercadoPagoExitoso = () => {
 
