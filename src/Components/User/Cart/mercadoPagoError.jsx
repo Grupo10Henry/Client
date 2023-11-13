af37476853ae98ff01a17c63e1c681468ca3666a
JE-FUNES <infoventacba@gmail.com>
2023-11-13 07:00:59 -0300

    mercadopago 3 landings + css

--- src/Components/User/Cart/mercadoPagoError.jsx
+++ src/Components/User/Cart/mercadoPagoError.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "../../../assets/logo_mi_butaca_color.svg";
 import { useSelector } from "react-redux";
 import {Link} from "react-router-dom";
-import styles from "./mercadoPago.module.css";
+import styles from "./mercadopago.module.css";
 
 const MercadoPagoError = () => {
     const userName = useSelector((state) => state.user.userInfo.name);
@@ -11,7 +11,7 @@ const MercadoPagoError = () => {
     return (
         <div className={styles.Page}>
         <div className={styles.ContainerGral}>
-            <img className="mx-auto h-12 w-auto" src={logo} alt="Mi Butaca" />
+            <img className={styles.logo} src={logo} alt="Mi Butaca" />
         <h1>Hubo un problema en el proceso de pago. </h1>
         <h2> Por favor, intenta nuevamente<span className={styles.Name}>{" "} {userName}</span>. </h2> 
         <h3> Ir a: {"   "}
--- src/Components/User/Cart/mercadoPagoPendiente.jsx
+++ src/Components/User/Cart/mercadoPagoPendiente.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "../../../assets/logo_mi_butaca_color.svg";
 import { useSelector } from "react-redux";
 import {Link} from "react-router-dom";
-import styles from "./mercadoPago.module.css";
+import styles from "./mercadopago.module.css";
 
 const MercadoPagoPendiente = () => {
     const userName = useSelector((state) => state.user.userInfo.name);
@@ -11,7 +11,7 @@ const MercadoPagoPendiente = () => {
     return (
         <div className={styles.Page}>
         <div className={styles.ContainerGral}>
-            <img className="mx-auto h-12 w-auto" src={logo} alt="Mi Butaca" />
+            <img className={styles.logo} src={logo} alt="Mi Butaca" />
         <h1>
             <span className={styles.Name}>{userName}</span>
             {", "} el proceso de pago está pendiente. </h1> 
