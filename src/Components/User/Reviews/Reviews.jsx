e35e3ed24c4a3172cd7aa19ab58bbf70d6a2c8af
francorinque <francoagustinrinquecorreo@gmail.com>
2023-11-14 15:13:55 -0300

    renderizo reviews del back

--- src/Components/User/Reviews/Reviews.jsx
+++ src/Components/User/Reviews/Reviews.jsx
@@ -4,13 +4,24 @@ import ReviewsUser from "./ReviewsUser/ReviewsUser"
 import ReviewsEvent from "./ReviewsEvent/ReviewsEvent"
 
 import style from "./Reviews.module.css"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 const Reviews = () => {
+  const [reviews, setReviews] = useState([])
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/review")
+      .then((res) => {
+        setReviews(res?.data)
+      })
+      .catch((error) => console.log(error))
+  }, [])
   return (
     <div>
       <h1 className={style.reviewTitle}>EVENTOS ANTERIORES</h1>
       <ReviewsEvent />
-      <ReviewsUser />
+      <ReviewsUser reviews={reviews} />
     </div>
   )
 }
