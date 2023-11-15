302e3aac5892dd20df0ee5854c9ad07fc36b1778
francorinque <francoagustinrinquecorreo@gmail.com>
2023-11-15 18:24:09 -0300

    ubicación en cards de desarrolladores

--- src/Components/User/DevelopersAbout/developers.js
+++ src/Components/User/DevelopersAbout/developers.js
@@ -2,6 +2,7 @@ const developers = [
   {
     id: 1,
     fullName: "Luis Carrillo",
+    location: "Bogotá, Colombia",
     photo: "/public/developers/luis.jpeg",
     linkedin: "https://www.linkedin.com/in/luis-carlos-carrillo-73aaa057/",
     github: "https://github.com/lccarrillo247",
@@ -9,6 +10,7 @@ const developers = [
   {
     id: 2,
     fullName: "Evelyn  Pereira",
+    location: "Buenos Aires, Argentina",
     photo: "/public/developers/evelyn.jpg",
     linkedin:
       "https://www.linkedin.com/in/evelyn-pereira-aa2843232?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
@@ -17,6 +19,7 @@ const developers = [
   {
     id: 3,
     fullName: "Franco Rinque",
+    location: "Buenos Aires, Argentina",
     photo: "/public/developers/franco.jpg",
     linkedin: "https://www.linkedin.com/in/franco-agustin-rinque/",
     github: "https://github.com/francorinque",
@@ -24,6 +27,7 @@ const developers = [
   {
     id: 4,
     fullName: "Julia Funes",
+    location: "Córdoba, Argentina",
     photo: "/public/developers/july.jpeg",
     linkedin: "https://www.linkedin.com/in/franco-agustin-rinque/",
     github: "https://github.com/JE-FUNES",
@@ -31,6 +35,7 @@ const developers = [
   {
     id: 5,
     fullName: "Francisco Pertile",
+    location: "Córdoba, Argentina",
     photo: "/public/developers/francisco.jpeg",
     linkedin: "https://www.linkedin.com/in/franpertile/",
     github: "https://github.com/FranciscoPer",
@@ -38,6 +43,7 @@ const developers = [
   {
     id: 6,
     fullName: "Lucia Vargas",
+    location: "Tucumán, Argentina",
     photo: "/public/developers/lucia.jpg",
     linkedin: "https://www.linkedin.com/in/analuciavargasl/",
     github: "https://github.com/Luvargaslombardi",
@@ -45,6 +51,7 @@ const developers = [
   {
     id: 7,
     fullName: "Maria Barrionuevo",
+    location: "Salta, Argentina",
     photo: "/public/developers/guada.jpeg",
     linkedin:
       "https://www.linkedin.com/in/mar%C3%ADa-guadalupe-redondo-barrionuevo-8b6174239/",
@@ -53,6 +60,7 @@ const developers = [
   {
     id: 8,
     fullName: "Kevin Ortíz",
+    location: "Santa Fe, Argentina",
     photo: "/public/developers/kevin.jpg",
     linkedin: "https://www.linkedin.com/in/kevin-ortiz-b29321276/",
     github: "https://github.com/kevinor03",
